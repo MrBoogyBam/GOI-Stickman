b9870139eb550bae18d5e94116f07097c5e8f0ea
MrBoogyBam <MrBoogyBamBusiness@gmail.com>
2023-03-07 18:56:40 +0200

    Cleaned up code

--- Plugin.cs
+++ Plugin.cs
@@ -227,11 +227,6 @@ namespace Stickman
 
                 for (int i = 0; i < colliders.Length; i++)
                 {
-                    for (int j = i + 1; j < colliders.Length; j++)
-                    {
-                        Physics2D.IgnoreCollision(colliders[i], colliders[j]);
-                    }
-
                     colliders[i].sharedMaterial = frictionMat;
                 }
 
